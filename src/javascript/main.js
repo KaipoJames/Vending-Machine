6e4ab52f3f06c9e4b1c4fa34efeccd1b0aae4937
Kaipo Wilmeth <kaipojames12@gmail.com>
2020-10-15 22:03:24 -1000

    Add Price To Labels

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -4,13 +4,16 @@ import { Vending } from "./vending.js";
 const machine = new Vending("Kaipo's Machine");
 machine.addDispenser('C1', 'pizza', 2.50, 10);
 machine.addDispenser('C2', 'burger', 2.50, 10);
-machine.addDispenser('A3', 'soda', 1.00, 10);
-machine.addDispenser('A4', 'water-bottle', 1.00, 10);
-machine.addDispenser('H5', 'bala', 1.25, 10);
-machine.addDispenser('H5', 'apple', 1.25, 10);
-machine.addDispenser('H5', 'banana', 1.25, 10);
-machine.addDispenser('H5', 'pear', 1.25, 10);
-machine.addDispenser('H5', 'mango', 1.25, 10);
+machine.addDispenser('C3', 'cookie', 1.50, 10);
+machine.addDispenser('K1', 'cheese', 1.75, 10);
+machine.addDispenser('A3', 'juice', 1.75, 10);
+machine.addDispenser('A4', 'water', 1.25, 10);
+machine.addDispenser('A2', 'soda', 1.00, 10);
+machine.addDispenser('Z3', 'bala', 1.00, 10);
+machine.addDispenser('H1', 'apple', 1.25, 10);
+machine.addDispenser('H2', 'banana', 1.25, 10);
+machine.addDispenser('H3', 'pear', 1.25, 10);
+machine.addDispenser('H4', 'mango', 1.25, 10);
 
 //machine.showContents();
 
@@ -30,16 +33,17 @@ const DispenserBuild = {
         for (let i=0; i < dispenserCount; i++) {
             const container = document.createElement("div");
             this.createDispenserImage(container, dispensers[i].src);
-            this.createDispenserLabel(container);
+            this.createDispenserLabel(container, dispensers[i]);
             container.classList.add("dispenser-container");
             container.classList.add("dispenser-container-" + i);
             dispensersContainer.appendChild(container);
         }
     },
     // Create A Label Box for each Dispenser
-    createDispenserLabel(container) {
+    createDispenserLabel(container, dispenserObj) {
         const dispenserLabel = document.createElement("div");
-        dispenserLabel.classList.add("dispenser-label")
+        dispenserLabel.innerHTML = dispenserObj.itemCode + " $" + dispenserObj.price;
+        dispenserLabel.classList.add("dispenser-label");
         container.appendChild(dispenserLabel);
     },
     // Create An Img for each Dispenser
