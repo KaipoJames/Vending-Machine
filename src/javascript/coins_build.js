231e5eb0a0a4b8b146ae129d4393229bf032bea3
Kaipo Wilmeth <kaipojames12@gmail.com>
2020-10-18 09:16:21 -1000

    Add Event Listeners for Coins

--- src/javascript/coins_build.js
+++ src/javascript/coins_build.js
@@ -1,10 +1,14 @@
 const coinsContainer = document.querySelector(".coins-container");
+let moneyString = document.querySelector("#current-money");
+let clearButton = document.querySelector("#clear-button");
 
 const coins = ["penny", "nickel", "dime", "quarter"];
+const coinValues = ["0.01", "0.05", "0.10", "0.25"];
 
 export const CoinsBuild = {
     init() {
         this.createCoinContainers();
+        this.addEventListeners();
     },
 
     createCoinContainers() {
@@ -25,7 +29,44 @@ export const CoinsBuild = {
     addCoinImage(container, coinName) {
         const coinImg = document.createElement("img");
         coinImg.classList.add("coinImg");
+        coinImg.classList.add("coinImg-" + coinName);
         coinImg.src = "/icon/coins/" + coinName + ".png"
         container.appendChild(coinImg);
+    },
+
+    addEventListeners() {
+        for (var coin of coins) {
+            const elName = "coinImg-" + coin;
+            const coinElement = document.querySelector("." + elName);
+            this.addCoinEventListener(coinElement, coin);
+        }
+        this.clearMoneyString();
+    },
+
+    addCoinEventListener(coinElement, coin) {
+        coinElement.addEventListener("click", () => {
+            console.log("clicked " + coin);
+            switch (coin) {
+                case "penny":
+                    moneyString.innerHTML = "$ " + 0.01.toFixed(2);
+                    break;
+                case "nickel":
+                    moneyString.innerHTML = "$ " + 0.05.toFixed(2);
+                    break;
+                case "dime":
+                    moneyString.innerHTML = "$ " + 0.10.toFixed(2);
+                    break;
+                case "quarter":
+                    moneyString.innerHTML = "$ " + 0.25.toFixed(2);
+                    break;
+            }
+        });
+    },
+
+    clearMoneyString() {
+        clearButton.addEventListener("click",() => {
+            moneyString.innerHTML = "$ " + 0.00.toFixed(2);
+        });
     }
+
 }
